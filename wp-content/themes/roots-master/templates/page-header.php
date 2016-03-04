c46a8bb3befacd7bde980075a2db13bdca8f5caa
bonbons0220 <bsouter@jhu.edu>
2016-03-04 16:33:25 -0500

    removed extraneous themes - roots_master

--- wp-content/themes/roots-master/templates/page-header.php
+++
@@ -1,9 +0,0 @@
-<div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
-  <?php if (is_page( array (386, 'help') )) :?>
-   <?php get_template_part('templates/searchform', 'page'); ?>
-            <?php endif;
-            ?>
-</div>
